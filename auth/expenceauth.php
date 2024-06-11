07c9121bb61af1c8b3a5c427d1c70a52e517700b
fahmid <dnetfahmidhasan@gmail.com>
2024-06-11 11:52:08 +0600

    insert data array to the database

--- auth/expenceauth.php
+++ auth/expenceauth.php
@@ -3,68 +3,40 @@
 
 
 if(isset($_POST['submit_exp_value'])){
-
-    foreach($_POST['item_title'] as $key => $value){
     $newDate = $_POST['item_date'];
-    $sql = "INSERT INTO expenses (date, title, amount, note) VALUES ('$newDate', :item_title, :item_amount, :item_note)"; 
-    $stmt = $conn->prepare($sql);
-    // print_r($stmt);
-    $stmt-> execute([
-        'item_title' => $_POST['item_title'][$key];
-        'item_amount' => $_POST['item_amount'][$key];
-        'item_note' => $_POST['item_note'][$key];
-        // 'item_title' => $_POST['item_amount'][$key];
-    ]);
+    $item_title = $_POST['item_title'];
+    $item_amount = $_POST['item_amount'];
+    $item_catagory = $_POST['item_catagory'];
+    $item_note = $_POST['item_note'];
 
-}
-
-    // $newDate = $_POST['item_date'];
-    // $item_title = $_POST['item_title'];
-    // $item_amount = $_POST['item_amount'];
-    // $item_catagory = $_POST['item_catagory'];
-    // $item_note = $_POST['item_note'];
-
-    // print_r($newDate);
-    // $item_title = $_POST['item_title'];
-    // print_r($item_title);
-    // foreach($item_title as $key => $title_each){
-    //     print_r($title_each)$key;
-    //     echo "<br>";
-    // }
-    // $item_amount = $_POST['item_amount'];
-    // print_r($item_amount);
-    // $item_catagory = $_POST['item_catagory'];
-    // print_r($item_catagory);
-    // $item_note = $_POST['item_note'];
-    // print_r($item_note);
-
-/*
-    if(!empty($newDate)){
-        $sql = "INSERT INTO expenses (date) VALUES ('$newDate')"; 
-    }
-    if(!empty($item_title)) {
-        foreach($item_title as $item_title_each){
-            $sql = "INSERT INTO expenses (title) VALUES ('$item_title_each')"; 
+    if(!empty($_POST['item_title'])) {
+        $title_res = "";
+        foreach($_POST['item_title'] as $item_title_each){
+            $title_res .= $item_title_each .",";
         }
     }
     if(!empty($item_amount)) {
+        $amount_res = '';
         foreach($item_amount as $item_amount_each){
-            $sql = "INSERT INTO expenses (amount) VALUES ('$item_amount_each')"; 
+            $amount_res .= (string)$item_amount_each .",";
+            print_r($amount_res);
         }
     }
     if(!empty($item_catagory)) {
+        $catagory_res = "";
         foreach($item_catagory as $item_catagory_each){
-            $sql = "INSERT INTO expenses (catagory) VALUES ('$item_catagory_each')"; 
+            $catagory_res .= $item_catagory_each .",";
         }
     }
     if(!empty($item_note)) {
+        $note_res = "";
         foreach($item_note as $item_note_each){
-            $sql = "INSERT INTO expenses (note) VALUES ('$item_note_each')"; 
-        }
+            $note_res .= $item_note_each .",";
+            }
     }
-    $sql_query = mysqli_query($conn,$sql);
+    // $sql = "INSERT INTO expenses (date, title, amount,catagory ,note) VALUES ('$newDate','$title_res','$amount_res','$catagory_res','$note_res')"; 
+    // $sql_query = mysqli_query($conn,$sql);
 
-*/
 
 
 }
